f9b3b733147c61bfaed8c1df488f228b3caa8220
nicolaiharbo <nicolaiharbo@10.50.137.58>
2015-09-08 09:38:15 +0200

    startServer metode oprettet

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -5,10 +5,21 @@
  */
 package server;
 
+import java.net.Socket;
+
 /**
  *
  * @author nicolaiharbo
  */
-public class ClientHandler {
+public class ClientHandler extends Thread {
+
+    ClientHandler(Socket socket, TCPServer server) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- src/server/TCPServer.java
+++ src/server/TCPServer.java
@@ -5,8 +5,13 @@
  */
 package server;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,27 +21,49 @@ public class TCPServer {
 
     private static Socket socket;
     private static ServerSocket serverSocket;
-    
-    private void stopServer(){
+    ArrayList<ClientHandler> clientList = new ArrayList();
+    private boolean keepRunning = true;
+
+    private void startServer() {
+        String ip = "localhost";
+        int port = 4321;
+
+        Logger.getLogger(TCPServer.class.getName()).log(Level.INFO, "Server started. Listening on: " + port + ", bound to: " + ip);
         
+        try {
+            serverSocket = new ServerSocket();
+            serverSocket.bind(new InetSocketAddress(ip, port));
+            do {
+                Socket socket = serverSocket.accept(); //Important Blocking call
+                Logger.getLogger(TCPServer.class.getName()).log(Level.INFO, "Connected to a client");
+                //Her startes et nyt objekt af typen clientHandle, som tager en socket og en server som argumenter.
+                ClientHandler ch = new ClientHandler(socket, this);
+                //Her tilføjes den nye client til listen over tilsluttede brugere.
+                clientList.add(ch);
+                //Her startes en ny tråd af typen clientHandle - dette kan gøres, fordi clienthandle extender Thread.
+                ch.start();
+
+            } while (keepRunning);
+        } catch (IOException ex) {
+            Logger.getLogger(TCPServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
-    
-    private void startServer(){
-        
+
+    private void stopServer() {
+
     }
-    
-    private void removeClient(){
-        
+
+    private void removeClient() {
+
     }
-    
-    public void send(){
-        
+
+    public void send() {
+
     }
-    
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
 
-
-    
 }
