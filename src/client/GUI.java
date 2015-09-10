9596834ff2e0ff9763f7becf4b6e2a369352ed64
hilleer <hiller@live.dk>
2015-09-10 14:28:35 +0200

    Ændring af opsætning i forhold til packages.

--- src/client/GUI.java
+++ src/client/GUI.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package view;
+package client;
 
 import client.TCPClient;
 import java.io.IOException;
